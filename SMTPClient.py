477af242e4a62bea62a9ac09c4e46a54ff2c6356
dakotanerd <79533638+dakotanerd@users.noreply.github.com>
2025-12-15 15:04:48 -0500

    Add files via upload

---
+++ SMTPClient.py
@@ -0,0 +1,86 @@
+import ssl
+import base64
+from socket import *
+
+# Email credentials
+sender_email = "" #replace with your own gmail
+receiver_email = "" #replace with your own email
+password = ""  #replace with your own App password for your gmail; Use an app password instead of your actual gmail password
+#create the App password here:
+#https://support.google.com/accounts/answer/185833?visit_id=638759601307026124-2089972828&p=InvalidSecondFactor&rd=1
+
+# Email content, replace with your own message and subject line
+msg = "I love computer networks!"    
+endmsg = "This is the end of the message"
+subject = "Greetings To you!"
+
+# specify Gmail SMTP server details
+mailserver = "smtp.gmail.com"
+smtp_port = 587
+
+# Create TCP socket (clientSocket) and establish a connection, and print out the response
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((mailserver,smtp_port))
+recv = clientSocket.recv(1024).decode()
+print(recv)
+
+# Secure connection with TLS by sending out Hello message (EHLO for TLS, not HELO) and print out the response
+clientSocket.send(b"EHLO smtp.gmail.com\r\n") 
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+
+#initiate a TLS (Transport Layer Security) handshake; STARTTLS command tells the server to start a TLS encryption
+clientSocket.send(b"STARTTLS\r\n")
+recv2 = clientSocket.recv(1024).decode()
+print(recv2)
+
+context = ssl.create_default_context()
+clientSocket = context.wrap_socket(clientSocket, server_hostname=mailserver)
+
+
+# Authenticate
+auth_command = "AUTH LOGIN\r\n"
+clientSocket.send(auth_command.encode())
+recv3 = clientSocket.recv(1024).decode()
+print(recv3)
+
+#send out email address for gmail
+clientSocket.send(base64.b64encode(sender_email.encode()) + b"\r\n")
+recv4 = clientSocket.recv(1024).decode()
+print(recv4)
+
+#send out password
+clientSocket.send(base64.b64encode(password.encode()) + b"\r\n")
+recv5 = clientSocket.recv(1024).decode()
+print(recv5)
+
+# Send MAIL FROM command, and print out the response
+clientSocket.send(f"MAIL FROM:<{sender_email}>\r\n".encode())
+recv6 = clientSocket.recv(1024).decode()
+print(recv6)  
+
+# Send RCPT TO command and print out the response 
+clientSocket.send(f"RCPT TO:<{receiver_email}>\r\n".encode())
+recv7 = clientSocket.recv(1024).decode()
+print(recv7)  
+
+# Send DATA command and print out the response 
+clientSocket.send(b"DATA\r\n")
+recv8 = clientSocket.recv(1024).decode()
+print(recv8)
+
+# Send email content, including subject, message, and endmsg, and print out the response 
+email_message = f"{subject}\r\n\r\n{msg}\r\n\r\n{endmsg}\r\n.\r\n"
+clientSocket.send(email_message.encode())
+recv9 = clientSocket.recv(1024).decode()
+print(recv9)
+
+
+# Send QUIT command and print out the response
+clientSocket.send(b"QUIT\r\n")
+recv10 = clientSocket.recv(1024).decode()
+print(recv10)  
+
+# Close client socket
+clientSocket.close()
+
